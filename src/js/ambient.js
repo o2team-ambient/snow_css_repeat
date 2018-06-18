a4db253ad19a27b0129f70ce83be71e3b9cf31ba
李怡欣 <liyixin@ECCMAC-C02WD3FC.local>
2018-06-18 20:18:22 +0800

    feat: 增加穿透点击属性与兼容

--- src/js/ambient.js
+++ src/js/ambient.js
@@ -4,6 +4,11 @@ import {
   O2_AMBIENT_MAIN
 } from './utils/const'
 
+const wrapper = document.querySelector('.o2team_ambient_main')
+wrapper.addEventListener('click', () => {
+  wrapper.style.display = 'none'
+})
+
 function initAmbient () {
   // let xxx = new XXX()
   // 主函数暴露
