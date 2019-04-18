ad6c89be38021345a73587e9779f108f0bf5979a
李怡欣 <liyixin@jd.com>
2019-04-18 14:09:25 +0800

    feat: 兼容包管理模式

--- src/js/ambient.js
+++ src/js/ambient.js
@@ -1,19 +1,24 @@
 import './utils/raf'
 import {
   O2_AMBIENT_INIT,
-  O2_AMBIENT_CONFIG,
-  O2_AMBIENT_MAIN,
+  O2_AMBIENT_MAIN
 } from './utils/const'
 import Ambient from './ambient/'
 
 // 判断是否可点，被点中则隐藏
-const wrapper = document.querySelector('.o2team_ambient_main')
+let wrapper = document.querySelector('.o2team_ambient_main')
+if (!wrapper) {
+  wrapper = document.createElement('div')
+  wrapper.setAttribute('class', 'o2team_ambient_main')
+  wrapper.setAttribute('id', 'o2team_ambient_main')
+  document.body.insertAdjacentElement('beforeend', wrapper)
+}
 wrapper.addEventListener('click', () => {
   wrapper.style.display = 'none'
 })
 
 // 初始化函数
-function initAmbient() {
+export default function initAmbient() {
   let ambient = new Ambient()
   // 主函数暴露
   window[O2_AMBIENT_MAIN] = ambient
@@ -21,14 +26,3 @@ function initAmbient() {
 
 // 初始化函数
 window[O2_AMBIENT_INIT] = initAmbient
-
-try {
-  // 保证配置读取顺序
-  let csi = setInterval(() => {
-    if (!window[O2_AMBIENT_CONFIG]) return
-    clearInterval(csi)
-    initAmbient()
-  }, 1000)
-} catch (e) {
-  console.log(e)
-}
